baf00c5f812043bbf386730984f474dd1dca122d
thomaslundberg44 <tlundberg44@gmail.com>
2015-08-18 15:56:40 +0100

    Auto-increment Customer ID - Add Customer
    Added Auto-increment feature when adding new customer.
    Class variable counts customer id.
    Edit button allows user to enter a different id if required

--- FCPbicycle/src/bicyclestore/cardlayouts/customercardlayouts/AddCustomerCard.java
+++ FCPbicycle/src/bicyclestore/cardlayouts/customercardlayouts/AddCustomerCard.java
@@ -1,6 +1,7 @@
 package bicyclestore.cardlayouts.customercardlayouts;
 
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.GridLayout;
@@ -8,6 +9,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
@@ -30,6 +32,10 @@ public class AddCustomerCard extends JPanel {
 	
 	private JTextField txtId, txtName, txtAddress, txtPhone, txtEmail;
 	private JButton btnAddCustomer;
+	
+	private JButton btnEditId;
+	private Box idPane;
+	private int idCounter = 5;
 
 	public AddCustomerCard(Database database, CustomersCardLayout cardLayout) {
 		this.database = database;
@@ -39,7 +45,8 @@ public class AddCustomerCard extends JPanel {
 	}
 	
 	private void initComponents() {
-		txtId = new JTextField(10);
+		setUpIdPane();
+		
 		txtName = new JTextField(10);
 		txtAddress = new JTextField(10);
 		txtPhone = new JTextField(10);
@@ -48,6 +55,22 @@ public class AddCustomerCard extends JPanel {
 		btnAddCustomer = new JButton("Add Customer");
 		btnAddCustomer.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnAddCustomer.addActionListener(new ButtonActionListener());
+	}
+	
+	private void setUpIdPane() {
+		idPane = Box.createHorizontalBox();
+		
+		// set ID field to auto-increment value, grey out colour and set editable to false
+		txtId = new JTextField(idCounter+"",10);
+		txtId.setBackground(Color.LIGHT_GRAY);
+		txtId.setEditable(false);
+		
+		btnEditId = new JButton("Edit ID");
+		
+		idPane.add(txtId);
+		idPane.add(btnEditId);
+		
+		btnEditId.addActionListener(new ButtonActionListener());
 	}
 	
 	private void createAddCustomerCard() {
@@ -68,7 +91,7 @@ public class AddCustomerCard extends JPanel {
 		customerDetailsGrid.setLayout(gridLayout);
 		customerDetailsGrid.setMaximumSize(new Dimension(400, 400));
 		customerDetailsGrid.add(new JLabel("Customers id"));
-		customerDetailsGrid.add(txtId);
+		customerDetailsGrid.add(idPane);
 		customerDetailsGrid.add(new JLabel("Customers name"));
 		customerDetailsGrid.add(txtName);
 		customerDetailsGrid.add(new JLabel("Customers address"));
@@ -79,6 +102,26 @@ public class AddCustomerCard extends JPanel {
 		customerDetailsGrid.add(txtEmail);
 		
 		return customerDetailsGrid;
+	}
+	
+	// check if all fields have been filled and attempt to add customer to database
+	private void checkFieldsComplete() {
+		// get customer details from text fields
+		String strId = txtId.getText();
+		String strName = txtName.getText();
+		String strAddress = txtAddress.getText();
+		String strPhone = txtPhone.getText();
+		String strEmail = txtEmail.getText();
+		
+		// check if any text fields have been left blank and output error message if true
+		if(strId.length() == 0 || strName.length() == 0 || strAddress.length() == 0 
+				|| strPhone.length() == 0 || strEmail.length() == 0) {
+			JOptionPane.showMessageDialog(null, "You must enter all customer details before submitting", 
+					"Incomplete content", JOptionPane.ERROR_MESSAGE);
+		}
+		// attempt to add customer if all fields are complete
+		else
+			addCustomer(strId, strName, strAddress, strPhone, strEmail);
 	}
 	
 	private void addCustomer(String strId, String name, String address, String phoneNum, String email) {
@@ -94,6 +137,9 @@ public class AddCustomerCard extends JPanel {
 					"Invalid content", JOptionPane.ERROR_MESSAGE);
 		}
 		if(gotId) {
+			// If the customer was added with auto-increment id, increment id for next customer 
+			if(customerId == idCounter)
+				idCounter++;
 			database.addCustomer(new Customer(customerId, name, address, phoneNum, email));
 			confirmCustomerAdded(strId, name, address, phoneNum, email);
 		}
@@ -104,7 +150,7 @@ public class AddCustomerCard extends JPanel {
 		JOptionPane.showMessageDialog(null, "New customer added,\nID: "+strId+", Name: "+name+", Address: "+address+
 				"\n Phone number: "+phone+", Email: "+email,
 				"Success", JOptionPane.INFORMATION_MESSAGE);
-		txtId.setText("");
+		txtId.setText(idCounter+"");
 		txtName.setText("");
 		txtAddress.setText("");
 		txtPhone.setText("");
@@ -116,23 +162,22 @@ public class AddCustomerCard extends JPanel {
 	private class ButtonActionListener implements ActionListener {
 
 		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// get customer details from text fields
-			String strId = txtId.getText();
-			String strName = txtName.getText();
-			String strAddress = txtAddress.getText();
-			String strPhone = txtPhone.getText();
-			String strEmail = txtEmail.getText();
+		public void actionPerformed(ActionEvent event) {
 			
-			// check if any text fields have been left blank and output error message if true
-			if(strId.length() == 0 || strName.length() == 0 || strAddress.length() == 0 
-					|| strPhone.length() == 0 || strEmail.length() == 0) {
-				JOptionPane.showMessageDialog(null, "You must enter all customer details before submitting", 
-						"Incomplete content", JOptionPane.ERROR_MESSAGE);
+			// if the user pressed the add customer button, check if fields are all filled and attempt to add customer
+			if(event.getSource() == btnAddCustomer) {
+				checkFieldsComplete();
 			}
-			// attempt to add customer if all fields are complete
-			else
-				addCustomer(strId, strName, strAddress, strPhone, strEmail);
+			// if the user pressed the edit id button display dialogue allowing them to edit id field
+			if(event.getSource() == btnEditId) {
+				try{
+					txtId.setText(Integer.parseInt(JOptionPane.showInputDialog(null, "Enter customer ID",
+							"Edit Customer ID", JOptionPane.INFORMATION_MESSAGE))+"");
+				}catch(NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "Customer ID must contain a number",
+							"Invalid Input", JOptionPane.ERROR_MESSAGE);
+				}
+			}
 		}
 		
 	}
