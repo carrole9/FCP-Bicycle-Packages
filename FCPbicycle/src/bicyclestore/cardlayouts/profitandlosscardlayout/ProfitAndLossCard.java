65ba3b139eb40a185cfada46746578224b556855
carrole9 <carrole9@tcd.ie>
2015-08-25 12:27:57 +0100

    update

---
+++ FCPbicycle/src/bicyclestore/cardlayouts/profitandlosscardlayout/ProfitAndLossCard.java
@@ -0,0 +1,170 @@
+
+	package bicyclestore.cardlayouts.profitandlosscardlayout;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.text.DecimalFormat;
+
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+
+import bicyclestore.Database;
+import bicyclestore.SystemData;
+//import bicyclestore.cardlayouts.profitandlosscardlayout.ProfitAndLossGraph.BarChartComponent;
+import bicyclestore.transaction.ProfitAndLoss;
+ 
+public class ProfitAndLossCard implements ItemListener {
+	
+    JPanel cardLayoutPane, cards; //a panel that uses CardLayout
+    final static String TEXTPANEL = "Profit And Loss Accounts";
+    final static String GRAPHPANEL = "Profit And Loss Graph";
+    final static String PredictionPANEL = "Sales Forcast";
+    JLabel expenditure, income, surplus, deficit, breakeven;
+	JLabel purchases, wages, bills, sales, totalexpenditure, totalincome;
+	private static Database data;
+	
+	public ProfitAndLossCard(Database data) {
+		this.data=data;
+		cardLayoutPane = new JPanel(new BorderLayout());
+		addComponentToPane(cardLayoutPane);
+	}
+     
+    public void addComponentToPane(Container pane) {
+    	ProfitAndLoss account= new ProfitAndLoss(data);
+    	account.updateAccounts();
+    	
+        JPanel comboBoxPane = new JPanel(); //use FlowLayout
+        String comboBoxItems[] = { TEXTPANEL, GRAPHPANEL };
+        JComboBox<String> cb = new JComboBox<String>(comboBoxItems);
+        cb.setEditable(false);
+        cb.addItemListener(this);
+        comboBoxPane.add(cb);
+         
+        //Create the "cards".
+        JPanel card1 = new JPanel();
+        JPanel card2 = new JPanel();
+        ProfitAndLossGraphIcon b = new ProfitAndLossGraphIcon(null, 0, 0, data);
+        card2.add(b.createAndShowGUI());
+        
+        JPanel card3 = new JPanel();
+         
+        //Create the panel that contains the "cards".
+        cards = new JPanel(new CardLayout());
+        cards.add(card1, TEXTPANEL);
+        cards.add(card2, GRAPHPANEL);
+     
+         
+        pane.add(comboBoxPane, BorderLayout.PAGE_START);
+        pane.add(cards, BorderLayout.CENTER);
+        
+        expenditure = new JLabel("--------EXPENDITURE----------- ");
+        purchases = new JLabel("Purchases from Supplier: ");
+        wages = new JLabel("Employees Wages: ");
+		bills = new JLabel("Bills:");
+		totalexpenditure = new JLabel("Total Expenditure:");
+		income = new JLabel("--------INCOME---------------- ");
+		sales = new JLabel("From sale of items: ");
+		totalincome = new JLabel("Total Income:");
+		surplus = new JLabel("--------BALANCE SURPLUS-------- ");
+		deficit = new JLabel("--------BALANCE DEFICIT-------- ");
+        DecimalFormat df = new DecimalFormat("0.00");
+        if(account.getOverallBalance()>0){
+        card1.add(expenditure);
+       	 expenditure.setBackground(Color.LIGHT_GRAY);
+       	 expenditure.setOpaque(true);
+        card1.add(purchases);
+        card1.add(new JTextField("€" + df.format(account.getPurchasingTransactionValue()), 15)).setFocusable(false);
+        card1.add(wages);
+        card1.add(new JTextField("€" + df.format(account.getEmployeeWages()), 15)).setFocusable(false);
+        card1.add(bills);
+	    card1.add(new JTextField("€" + df.format(account.getBills()), 15));
+	    card1.add(totalexpenditure);
+	    card1.add(new JTextField("€" + df.format(account.getTotalExpenditure()), 15)).setFocusable(false);
+		card1.add(income );
+		card1.add(sales);
+		card1.add(new JTextField("€" + df.format(account.getTotalIncome()), 15)).setFocusable(false);
+		card1.add(totalincome);
+	    card1.add(new JTextField("€" + df.format(account.getTotalIncome()), 15)).setFocusable(false);
+		card1.add(surplus);
+		card1.add(new JTextField("€" + df.format(account.getOverallBalance()), 15)).setFocusable(false);
+		card1.setLayout(new BoxLayout(card1, BoxLayout.Y_AXIS));
+        }
+        
+        else if(account.getOverallBalance()<0){
+        	card1.add(expenditure);
+        	expenditure.setBackground(Color.LIGHT_GRAY);
+        	expenditure.setOpaque(true);
+            card1.add(purchases);
+            card1.add(new JTextField("€" + df.format(account.getPurchasingTransactionValue()), 15)).setFocusable(false);
+            card1.add(wages);
+            card1.add(new JTextField("€" + df.format(account.getEmployeeWages()), 15)).setFocusable(false);
+            card1.add(bills);
+     	    card1.add(new JTextField("€" + df.format(account.getBills()), 15)).setFocusable(false);
+     	    card1.add(totalexpenditure);
+   	        card1.add(new JTextField("€" + df.format(account.getTotalExpenditure()), 15)).setFocusable(false);
+     		card1.add(income );
+     		income.setBackground(Color.LIGHT_GRAY);
+       	    income.setOpaque(true);
+     		card1.add(sales);
+     		card1.add(new JTextField("€" + df.format(account.getTotalIncome()), 15)).setFocusable(false);
+     		card1.add(totalincome);
+   	        card1.add(new JTextField("€" + df.format(account.getTotalIncome()), 15)).setFocusable(false);
+    		card1.add(deficit);
+    		deficit.setBackground(Color.LIGHT_GRAY);
+    		deficit.setOpaque(true);
+    		card1.add(new JTextField("€" + df.format(account.getOverallBalance()), 15)).setFocusable(false);
+    		card1.setLayout(new BoxLayout(card1, BoxLayout.Y_AXIS));
+        }
+        else if(account.getOverallBalance()==0){
+        	 card1.add(expenditure);
+        	 expenditure.setBackground(Color.LIGHT_GRAY);
+         	 expenditure.setOpaque(true);
+             card1.add(purchases);
+             card1.add(new JTextField("€" + df.format(account.getPurchasingTransactionValue()), 15)).setFocusable(false);
+             card1.add(wages);
+             card1.add(new JTextField("€" + df.format(account.getEmployeeWages()), 15)).setFocusable(false);
+             card1.add(bills);
+     	    card1.add(new JTextField("€" + df.format(account.getBills()), 15));
+     	    card1.add(totalexpenditure);
+   	        card1.add(new JTextField("€" + df.format(account.getTotalExpenditure()), 15)).setFocusable(false);
+     		card1.add(income );
+     		income.setBackground(Color.LIGHT_GRAY);
+       	    income.setOpaque(true);
+     		card1.add(sales);
+     		card1.add(new JTextField("€" + df.format(account.getTotalIncome()), 15)).setFocusable(false);
+     		card1.add(totalincome);
+   	        card1.add(new JTextField("€" + df.format(account.getTotalIncome()), 15)).setFocusable(false);
+    		card1.add(breakeven);
+    		breakeven.setBackground(Color.LIGHT_GRAY);
+    		breakeven.setOpaque(true);
+    		card1.add(new JTextField("€" + df.format(account.getOverallBalance()), 15)).setFocusable(false);
+    		card1.setLayout(new BoxLayout(card1, BoxLayout.Y_AXIS));
+        }
+        
+   
+    
+        
+    }
+     
+    public void itemStateChanged(ItemEvent evt) {
+        CardLayout cl = (CardLayout)(cards.getLayout());
+        cl.show(cards, (String)evt.getItem());
+    }
+     
+  
+    public static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("Profit and Loss Accounts");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //ProfitAndLossCard demo = new ProfitAndLossCard(data);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    public JPanel getStockControlCardLayout() {
+    	return cardLayoutPane;
+    }
+    
+
+}
---
+++ FCPbicycle/src/bicyclestore/cardlayouts/profitandlosscardlayout/ProfitAndLossGraph.java
@@ -0,0 +1,212 @@
+package bicyclestore.cardlayouts.profitandlosscardlayout;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.text.DecimalFormat;
+
+import javax.swing.Icon;
+import javax.swing.JFrame;
+
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.border.*;
+
+import bicyclestore.Database;
+import bicyclestore.SystemData;
+import bicyclestore.transaction.ProfitAndLoss;
+import bicyclestore.transaction.StockControl;
+//import bicyclestore.transaction.profitControl;
+
+public class ProfitAndLossGraph extends JPanel
+{
+    private int graphHeight = 200;
+    private int barWidth = 50;
+    private int barGap = 25;
+
+    private JPanel barPanel;
+    private JPanel labelPanel;
+    
+    private Database data;
+
+    private List<Bar> bars = new ArrayList<Bar>();
+
+    public ProfitAndLossGraph(Database data){
+    	this.data=data;
+    	setUpGraph();
+    	
+    }
+    public void setUpGraph(){
+    	this.data=data;
+    	setBorder( new EmptyBorder(0, 0, 0, 0) );
+       // setBorder( new EmptyBorder(10, 10, 10, 10) );
+        setLayout( new BorderLayout() );
+
+        barPanel = new JPanel( new GridLayout(1, 0, barGap, 0) );
+        Border inside= new EmptyBorder(0, 0, 0, 0);
+        Border outside = new MatteBorder(1, 1, 1, 1, Color.BLACK);
+        Border compound = new CompoundBorder(outside, inside);
+        barPanel.setBorder(compound);
+
+        labelPanel = new JPanel( new GridLayout(1, 0, barGap, 0) );
+        labelPanel.setBorder( new EmptyBorder(10, 10, 0, 10) );
+
+        add(barPanel, BorderLayout.CENTER);
+        add(labelPanel, BorderLayout.PAGE_END);
+    }
+
+    public void addColumn(String label, double value, Color color)
+    {
+        Bar bar = new Bar(label, value, color);
+        bars.add( bar );
+    }
+
+    public void layoutGraph()
+    {
+        barPanel.removeAll();
+        labelPanel.removeAll();
+
+        int maxValue = 0;
+
+        for (Bar bar: bars)
+            maxValue = (int) Math.max(maxValue, bar.getValue());
+
+        for (Bar bar: bars)
+        {
+            JLabel tag = new JLabel("");
+            tag.setHorizontalTextPosition(JLabel.CENTER);
+            tag.setHorizontalAlignment(JLabel.CENTER);
+            tag.setVerticalTextPosition(JLabel.TOP);
+            tag.setVerticalAlignment(JLabel.BOTTOM);
+            double barHeight = (bar.getValue() * graphHeight) / maxValue;
+            ProfitAndLossGraphIcon icon = new ProfitAndLossGraphIcon(bar.getColor(), barWidth, barHeight, data);
+            tag.setIcon( icon );
+            barPanel.add( tag );
+
+            JLabel barLabel = new JLabel( bar.getLabel() );
+            barLabel.setHorizontalAlignment(JLabel.CENTER);
+            labelPanel.add( barLabel );
+            
+            
+           
+        }
+    }
+
+    private class Bar
+    {
+        private String tag;
+        private double value;
+        private Color color;
+
+        public Bar(String tag, double value, Color color)
+        {
+            this.tag = tag;
+            this.value = value;
+            this.color = color;
+        }
+
+        public String getLabel()
+        {
+            return tag;
+        }
+       
+
+        public double getValue()
+        {
+            return value;
+        }
+
+        public Color getColor()
+        {
+            return color;
+        }
+    }
+
+}
+
+ class ProfitAndLossGraphIcon implements Icon
+    {
+	 	private Color color;
+        private int width;
+        private double height;
+        private Database data;
+
+        public ProfitAndLossGraphIcon(Color color, int width, double barHeight, Database data)
+        {
+            this.color = color;
+            this.width = width;
+            this.height = barHeight;
+            this.data=data;
+        }
+
+        public int getIconWidth()
+        {
+            return width;
+        }
+
+        public int getIconHeight()
+        {
+            return (int) height;
+        }
+
+        public void paintIcon(Component c, Graphics g, int x, int y)
+        {
+           
+        	g.setColor(color);
+            g.fillRect(x, y, width, (int) height);
+            g.setColor(Color.BLACK);
+            g.fillRect(x + width , y , 0, (int) height);
+        }
+    
+
+    public Component createAndShowGUI()
+    {   ProfitAndLoss profit = new ProfitAndLoss(data);
+    	profit.updateAccounts();
+    	ProfitAndLossGraph panel = new ProfitAndLossGraph(data);
+    	DecimalFormat df = new DecimalFormat("0.00");
+        double first=(int) Math.round(profit.getTotalExpenditure());
+        double second=(int) Math.round(profit.getTotalIncome());
+        double third=(int) profit.getOverallBalance();
+        String expenditureAmount= df.format(profit.getTotalExpenditure());
+        String incomeAmount=df.format(profit.getTotalIncome());
+        String balanceAmount=df.format(profit.getOverallBalance());
+               
+        if(profit.getOverallBalance()>0){
+        	String one = "Total Expenditure: € " + expenditureAmount;
+        	String two = "Total Income: €" + incomeAmount;
+        	String three = "Overall Profit: €" + balanceAmount;
+        	panel.addColumn(one, first, Color.RED);
+            panel.addColumn(two, second, Color.GREEN);
+            panel.addColumn(three, third, Color.GREEN);
+        }
+        else if(profit.getOverallBalance()<0){
+            third= Math.abs(third);
+            String one = "Total Expenditure: €" + first;
+        	String two = "Total Income: €" + incomeAmount;
+        	String three = "Overall Loss: €" + third;
+            panel.addColumn(one, first, Color.RED);
+            panel.addColumn(two, second, Color.GREEN);
+            panel.addColumn(three, third, Color.RED);
+        	
+        }
+        else if(profit.getOverallBalance()==0){
+        	 third= Math.abs(third);
+             String one = "Total Expenditure: €" + expenditureAmount;
+         	String two = "Total Income: €" + incomeAmount;
+         	String three = "Break Even: " + balanceAmount;
+             panel.addColumn(one, first, Color.RED);
+             panel.addColumn(two, second, Color.GREEN);
+             panel.addColumn(three, third, Color.RED);
+        }
+        
+        
+       
+        
+        panel.layoutGraph();
+		return panel;	
+    }
+
+    }
